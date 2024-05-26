b879a7757d435398b1a4b5780a3f873bbabd3b6c
dinhhuynhthaibinh123 <binhdinhqt137@gmail.com>
2024-05-26 23:09:12 +0700

    feat: handle upload multiple images

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+
+import { Heading } from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { ProductColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
+
+interface ProductClientProps {
+    data: ProductColumn[];
+}
+
+export const ProductClient: React.FC<ProductClientProps> = ({ data }) => {
+    const router = useRouter();
+    const params = useParams();
+
+    return (
+        <>
+            <div className="flex items-center justify-between">
+                <Heading
+                    title={`Products (${data.length || 0})`}
+                    description="Manage products for yours store"
+                />
+                <Button
+                    onClick={() =>
+                        router.push(`/${params.storeId}/products/new`)
+                    }
+                >
+                    <Plus className="mr-2 h-4 w-4" />
+                    Add New
+                </Button>
+            </div>
+
+            <Separator />
+
+            <DataTable columns={columns} data={data} searchKey="label" />
+
+            <Heading title="API" description="API for products" />
+
+            <Separator />
+
+            <ApiList entityName="products" entityIdName="productId" />
+        </>
+    );
+};
